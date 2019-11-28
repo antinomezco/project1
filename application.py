2f761158a7031380c9dc6b2d64702742d014ebbe
Constantino Saldana <constantino.saldana@gmail.com>
2019-11-28 18:15:19 +0000

    registration, login, logout, import and session work

--- application.py
+++ application.py
@@ -8,8 +8,6 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-###app.secret_key = 'YouWillNeverGuessMySecretKey'
-
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -23,25 +21,26 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-@app.route("/")
-def index():
-    title = "Home"
-    return render_template("home.html", title=title)
+#def check_login():
+#    if 'username' in session:
+#        s = session['username'];
+#        return render_template('search.html', title=title, s = s)
 
+@app.route("/")
 @app.route("/login", methods=["GET","POST"])
 def login_page():
     rmessage = None
     if request.method == 'POST':
-        url = request.headers.get("Referer")
+        url = request.headers.get("Referer") #Gets the page where you're coming from, in this case, if you're coming from the register page
         if url == "https://antinomy.pythonanywhere.com/register":
             rmessage = "Thanks for registering, please log in"
-        username = request.form.get("username")
-        password = request.form.get("password")
-        password_confirm = request.form.get("password_confirm")
+        username = request.form.get("username") #gets the name from the form
+        password = request.form.get("password") #same, but for the password
+        password_confirm = request.form.get("password_confirm") #confirms if the password is repeated correctly
         if password != password_confirm:
             return render_template("error.html", message="Password mismatch.")
-        db.execute("INSERT INTO username_table VALUES (:username, :password)", {"username": username, "password": password})
-        db.commit()
+        db.execute("INSERT INTO username_table VALUES (:username, :password)", {"username": username, "password": password}) #inserts the username and pass onto the db
+        db.commit() #executes the prev INSERT command
     title = "Login"
     return render_template("login.html", title=title, rmessage=rmessage)
 
@@ -52,9 +51,13 @@ def search_page():
         username = request.form.get("username")
         password = request.form.get("password")
         user_match = db.execute("SELECT * FROM username_table WHERE username = :username AND password = :password", {"username": username, "password": password}).fetchone()
+        session['username']=username
         if user_match is None:
             return render_template("error.html", message="Incorrect username and/or password.")
-    return render_template("search.html", title=title)
+    if 'username' in session:
+        s = session['username'];
+        return render_template('search.html',s = s)
+    return render_template("search.html", title=title, s=s)
 
 @app.route("/register")
 def register_page():
@@ -64,13 +67,21 @@ def register_page():
 @app.route("/book")
 def book_page():
     title = "Book"
-    return render_template("book.html", title=title)
+    if 'username' in session:
+        s = session['username'];
+        return render_template('book.html', title=title, s = s)
+    return render_template("error.html", message="Please log in.")
 
 @app.route("/review")
 def review_page():
     title = "Review"
-    return render_template("review.html", title=title)
+    if 'username' in session:
+        s = session['username'];
+        return render_template('review.html', title=title, s = s)
+    return render_template("error.html", message="Please log in.")
 
 @app.route("/logout")
 def logout_page():
-    return render_template("logout.html")
+    if 'username' in session:
+        session.pop('username',None)
+        return render_template('logout.html')
