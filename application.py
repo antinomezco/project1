2f6c7a33ed7804dff5ada8c2701265ff65372082
Constantino Saldana <constantino.saldana@gmail.com>
2019-12-05 20:45:23 +0000

    finished with functionality and basic looks, missing security

--- application.py
+++ application.py
@@ -59,8 +59,8 @@ def search_page():
             return render_template("error.html", message="Incorrect username and/or password.")
     if 'username' in session:
         s = session['username'];
-        return render_template('search.html', s = s)
-    return render_template("search.html", title=title, s=s)
+        return render_template('search.html', s = s, title=title)
+    return render_template("error.html", message="Please log in.")
 
 @app.route("/book", methods=["GET","POST"])
 def book_page():
@@ -88,11 +88,11 @@ def book_details(isbn_num):
     title = "Book details"
     if 'username' in session:
         s = session['username']
+        goodreads_data = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": grkey, "isbns": isbn_num}).json()
         book_data = db.execute("SELECT books_table.isbn, books_table.title, books_table.author, books_table.year, review_table.review, review_table.rating, review_table.isbn FROM books_table LEFT JOIN review_table ON books_table.isbn = review_table.isbn WHERE books_table.isbn = :isbn AND review_table.user_id = :user_id", {"isbn": isbn_num, "user_id": session['user_id']}).fetchone()
         if book_data is None:
             book_data = db.execute("SELECT books_table.isbn, books_table.title, books_table.author, books_table.year FROM books_table WHERE books_table.isbn = :isbn", {"isbn": isbn_num, "user_id": session['user_id']}).fetchone()
         if request.headers.get("Referer") == "https://antinomy.pythonanywhere.com/book":
-            goodreads_data = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": grkey, "isbns": isbn_num}).json()
             return render_template('book_details.html', title=title, s = s,
             book_data=book_data, goodreads_data=goodreads_data, isbn_num=isbn_num)
         if request.method == 'GET':
@@ -102,7 +102,8 @@ def book_details(isbn_num):
                     "author":book_data.author,
                     "year":book_data.year, "isbn":book_data.isbn,
                     "review_count":goodreads_data["books"][0]["ratings_count"],
-                    "average_rating":goodreads_data["books"][0]["average_rating"]})
+                    "average_rating":goodreads_data["books"][0]["average_rating"]
+                    })
             except:
                 abort(404)
     return render_template("error.html", message="Please log in.", title=title)
